948fdfa02758d16435ffa6a2b4e704ed04319969
hien123454 <ngochien15092000@gmail.com>
2021-10-06 11:22:14 +0700

    update khoa-service

--- src/main/java/com/example/khoaservice/controller/KhoaController.java
+++ src/main/java/com/example/khoaservice/controller/KhoaController.java
@@ -4,6 +4,7 @@ import com.example.khoaservice.entity.Khoa;
 import com.example.khoaservice.service.KhoaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,22 +13,20 @@ import org.springframework.web.bind.annotation.*;
 public class KhoaController {
     @Autowired
     private KhoaService khoaService;
-
-
+    @Value("${welcome}")
+    String AA;
     @PostMapping("/")
     public Khoa saveKhoa(@RequestBody Khoa khoa){
         log.info("inside saveDeparment method if Department controller!");
         return khoaService.saveKhoa(khoa);
     }
-
     @GetMapping("/{id}")
     public Khoa findKhoaById(@PathVariable("id") Long khoaId){
         log.info("inside findDeparmentById method if Department controller!");
         return khoaService.findKhoaById(khoaId);
     }
-
     @GetMapping("/")
     public String helloWorld(){
-        return "Hello Nguyen Trong Khoi!";
+        return AA;
     }
 }
